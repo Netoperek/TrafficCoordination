6921cd7b512f8963a62fef78b2d30809610c2d2a
Netoperek <piotrkala92@gmail.com>
2016-01-31 18:05:04 +0100

    Improving simulator for multicrossroads

--- states_to_graphs.rb
+++ states_to_graphs.rb
@@ -37,18 +37,44 @@ def base_json(cars_states, roads_states, colors_cars_hash)
     road_nr = road[:road_nr]
     car_state = cars_states.select { |ele| ele.state[:current_road_nr] == road_nr }
 
-    while size > 0
+    i = 0
+    while i < size
       car_nr = nil
       car_nr = car_state.first.state[:car_nr] unless car_state.first.nil?
-      node = { :name => size, :road_nr => road[:road_nr], :car_nr => car_nr } 
+      node = { :name => i+1, :road_nr => road[:road_nr], :car_nr => car_nr } 
       nodes.push(node)
-      size -= 1
+      i += 1
     end
   end
 
+  crossing_nodes = Set.new
+  crossroads = roads_states.map { |ele| { :road_nr => ele.state[:road_nr] , :cuts => ele.state[:cuts] } }
+  crossroads.each do |ele|
+    road_nr = ele[:road_nr]
+    cuts = ele[:cuts]
+    cuts.each do |cut|
+      crossing_road_nr = cut[:road_nr]
+      node_a = nodes.find_index { |node| node[:name] == cut[:crossroad] && node[:road_nr] == road_nr }
+      crossroad = crossroads.select { |cross| cross[:road_nr] == crossing_road_nr }
+      cut = crossroad.first[:cuts].select { |ele| ele[:road_nr] == road_nr }
+      cut = cut.first[:crossroad]
+      node_b = nodes.find_index { |node| node[:name] == cut && node[:road_nr] == crossing_road_nr}
+      crossing_nodes.add([node_b, node_a].sort)
+    end
+  end
+
+  crossroads_indices = []
+  crossing_nodes.each do |ele|
+    nodes.delete_at(ele[0])
+    nodes.delete_at(ele[1]-1)
+    node = { :name => "#{ele[0]-1}-#{ele[1]-2}", :road_nr => "#{ele[0]}-#{ele[1]}", :car_nr => -1 } 
+    nodes.push(node)
+    links.delete_if { |e| e[:source] == ele[0] || e[:target] == ele[0] }
+    links.delete_if { |e| e[:source] == ele[1] || e[:target] == ele[1] }
+    crossroads_indices.push(nodes.size-1)
+  end
+
   i = -1
-  crossing_nodes = {}
-  crossroads_hash = {}
   while i < nodes.size-2
     i += 1
     node = nodes[i]
@@ -56,48 +82,24 @@ def base_json(cars_states, roads_states, colors_cars_hash)
     node_road_nr = node[:road_nr]
     next_node_road_nr = next_node[:road_nr]
 
-    if node_road_nr == next_node_road_nr
+    if node_road_nr == next_node_road_nr && node[:name] == next_node[:name]-1
       link = { :source => i, :target => i+1 }
       links.push(link)
     end
   end
 
-  crossroads = roads_states.map { |ele| { :road_nr => ele.state[:road_nr] , :cuts => ele.state[:cuts] } }
-  crossroads.each do |ele|
-    road_nr = ele[:road_nr]
-    cuts = ele[:cuts]
-    cuts.each do |cut|
-      crossing_road_nr = cut[:road_nr]
-      node_a = nodes.select { |node| node[:name] == cut[:crossroad] && node[:road_nr] == road_nr }
-      node_a = node_a.first
-      crossroad = crossroads.select { |cross| cross[:road_nr] == crossing_road_nr }
-      cut = crossroad.first[:cuts].select { |ele| ele[:road_nr] == road_nr }
-      cut = cut.first[:crossroad]
-      node_b = nodes.select { |node| node[:name] == cut && node[:road_nr] == crossing_road_nr}
-      crossing_nodes[node_a] = node_b
-    end
-  end
-
-  crossing_nodes.values.each do |i|
-    road = roads_states.select { |ele| ele.state[:road_nr] == nodes[i][:road_nr] }
-    cuts = road.first.state[:cuts]
-    cuts.push(road.first.state[:road_nr])
-    crossroads = cuts.sort.join(" ")
-    unless crossroads_set.include? crossroads
-      crossroads_set.add(crossroads)
-      node = { :road_nr => crossroads, :name => 0 }
-      nodes.push(node)
-      crossroads_hash[crossroads] = nodes.size-1
-    end
-  end
-
-  crossroads_set.each do |ele|
-    ele.split().each do |node|
-      target = crossing_nodes[node.to_i]
-      target = crossing_nodes[-node.to_i] if target.nil?
-      link = { :source => crossroads_hash[ele], :target => target }
-      links.push(link)
-    end
+  crossroads_indices.each do |ele|
+    crossroad_node = nodes[ele][:name]
+    node_a = crossroad_node.split('-')[0].to_i
+    node_b = crossroad_node.split('-')[1].to_i
+    link = { :source => node_a, :target => ele }
+    links.push(link)
+    link = { :source => ele, :target => node_a+1 }
+    links.push(link)
+    link = { :source => node_b, :target => ele }
+    links.push(link)
+    link = { :source => ele, :target => node_b+1 }
+    links.push(link)
   end
 
   result = { 'nodes' => nodes, 'links' => links }
@@ -117,6 +119,7 @@ def print_graph(outcome, index, colors_roads_hash, colors_cars_hash)
   g = GraphViz.new(:G, :type => :digraph, :use => 'neato')
   nodes = []
   color = 'black'
+
   outcome["nodes"].each do |node|
     if node[:road_nr].is_a?(Integer) && !colors_roads_hash.keys.include?(node[:road_nr])
       color = random_hex_color(colors_set)
@@ -124,15 +127,15 @@ def print_graph(outcome, index, colors_roads_hash, colors_cars_hash)
     elsif node[:road_nr].is_a?(Integer)
       color = colors_roads_hash[node[:road_nr]]
     end
-    node_name_suffix = node[:road_nr].to_s
-    node_name_suffix = node[:road_nr].to_s if node[:road_nr].is_a?(Integer)
+
     unless node[:car_nr].nil?
       car_color = colors_cars_hash[node[:car_nr]]
       nodes.push(g.add_nodes('car#' + node[:car_nr].to_s, :color => car_color, :fillcolor => color, :style => :filled, :penwidth => 16.0, :label => node[:car_nr].to_s))
     else
-      nodes.push(g.add_nodes(node[:name].to_s + "#" + node_name_suffix, :color => 'black', :fillcolor => color, :style => :filled, :penwidth => 1.0, :label => '' ))
+      nodes.push(g.add_nodes(node[:name].to_s + '#' + node[:road_nr].to_s, :color => 'black', :fillcolor => color, :style => :filled, :penwidth => 1.0, :label => ''))
     end
   end
+
   outcome["links"].each do |link|
     source = link[:source]
     target = link[:target]
@@ -153,15 +156,7 @@ def apply_changing_states(core_outcome, graph, colors_cars_hash)
     graph["nodes"].each do |node|
       node[:car_nr] = nil
       states.each do |car_state|
-        if car_state["current_road_nr"] == node[:road_nr] && car_state["position"] == node[:name]
-          node[:car_nr] = car_state['car_nr']
-        end 
-        if node[:road_nr].is_a?(String) 
-          is_on_crossroads = node[:road_nr].split().include?(car_state["current_road_nr"].to_s) && node[:road_nr].split().include?(car_state["final_road_nr"].to_s)
-          if is_on_crossroads && car_state["position"] == node[:name]
-            node[:car_nr] = car_state['car_nr']
-          end 
-        end
+        node[:car_nr] = car_state['car_nr'] if car_state["current_road_nr"] == node[:road_nr] && car_state["position"] == node[:name]
       end
     end
     print_graph(graph, index, colors_roads_hash, colors_cars_hash)
