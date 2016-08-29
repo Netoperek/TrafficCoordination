0c017f8024d4ec728408fc7ebce7481a67cd90e7
Netoperek <piotrkala92@gmail.com>
2016-08-29 21:06:17 +0200

    Fixing runner

--- traffic_coordination_system/src/runner.sh
+++ traffic_coordination_system/src/runner.sh
@@ -1,10 +1,10 @@
-for i in {2,4,6,8,10,12,14}; do
+for i in {2,4,6,8}; do
   rm ../../statistics_output/cars_count_$i
 done
 
-for j in {2,4,6,8,10,12,14}; do
+for j in {2,4,6,8}; do
   for i in {1..30}; do
-    echo "case $$" >> ../../statistics_output/cars_count_$j
+    echo "case $i" >> ../../statistics_output/cars_count_$j
     ruby main.rb ../inputs/generated6/generated_cars_states_file$i ../inputs/sl_roads_file >> ../../statistics_output/cars_count_$j
     echo "" >> ../../statistics_output/cars_count_$j
   done
