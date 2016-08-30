52c9ddf897a0871a78537c321dbad828869d1043
Piotr Kala <PiotrKala@pega.com>
2016-08-30 08:57:39 +0200

    Improving runner

--- traffic_coordination_system/src/runner.sh
+++ traffic_coordination_system/src/runner.sh
@@ -1,11 +1,13 @@
-for i in {2,4,6,8}; do
+for i in {2,4,6,8,10}; do
+#for i in {12,14}; do
   rm ../../statistics_output/cars_count_$i
 done
 
-for j in {2,4,6,8}; do
+for j in {2,4,6,8,10}; do
+#for j in {12,14}; do
   for i in {1..30}; do
     echo "case $i" >> ../../statistics_output/cars_count_$j
-    ruby main.rb ../inputs/generated6/generated_cars_states_file$i ../inputs/sl_roads_file >> ../../statistics_output/cars_count_$j
+    ruby main.rb ../inputs/generated$j/generated_cars_states_file$i ../inputs/sl_roads_file >> ../../statistics_output/cars_count_$j
     echo "" >> ../../statistics_output/cars_count_$j
   done
 done
